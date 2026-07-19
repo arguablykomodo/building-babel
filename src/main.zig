2d844b4c7742d7a467e5c8a13956534dc739576a
Komodo <noreply@komodo.ar>
2026-07-18 22:06:36 -0300

    feat: basic block placing

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,43 @@
 const w4 = @import("wasm4.zig");
 
-const smiley = [8]u8{
-    0b11000011,
-    0b10000001,
-    0b00100100,
-    0b00100100,
-    0b00000000,
-    0b00100100,
-    0b10011001,
-    0b11000011,
-};
+const BLOCK_SIZE = 8;
+const WIDTH = 10;
+const HEIGHT = 20;
+
+var grid = [_][WIDTH]bool{[_]bool{false} ** WIDTH} ** HEIGHT;
+var x: u8 = 0;
+var y: u8 = 0;
+var prev_input: u8 = 0;
 
 export fn start() void {}
 
 export fn update() void {
     w4.DRAW_COLORS.* = 2;
-    w4.text("Hello from Zig!", 10, 10);
 
-    const gamepad = w4.GAMEPAD1.*;
-    if (gamepad & w4.BUTTON_1 != 0) {
-        w4.DRAW_COLORS.* = 4;
+    const input = w4.GAMEPAD1.*;
+    const new_input = input & (input ^ prev_input);
+    prev_input = input;
+
+    if (new_input & w4.BUTTON_RIGHT != 0 and x < (WIDTH - 1)) x += 1;
+    if (new_input & w4.BUTTON_LEFT != 0 and x > 0) x -= 1;
+    if (new_input & w4.BUTTON_DOWN != 0 and y < (HEIGHT - 1)) y += 1;
+    w4.rect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+
+    if (new_input & w4.BUTTON_1 != 0) {
+        while (y < (HEIGHT - 1) and !grid[y+1][x]) y += 1;
+        grid[y][x] = true;
+        y = 0;
     }
 
-    w4.blit(&smiley, 76, 76, 8, 8, w4.BLIT_1BPP);
-    w4.text("Press X to blink", 16, 90);
+    var grid_y: u8 = 0;
+    for (grid) |row| {
+        var grid_x: u8 = 0;
+        for (row) |cell| {
+            if (cell) {
+                w4.rect(grid_x * BLOCK_SIZE, grid_y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+            }
+            grid_x += 1;
+        }
+        grid_y += 1;
+    }
 }
