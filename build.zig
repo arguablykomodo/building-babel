f7bd96f5efd6538411b967ce9edf516570f4dbc2
Komodo <noreply@komodo.ar>
2026-07-19 00:10:59 -0300

    feat: zig check step

--- build.zig
+++ build.zig
@@ -1,25 +1,25 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    const exe = b.addExecutable(.{
-        .name = "cart",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = b.resolveTargetQuery(.{
-                .cpu_arch = .wasm32,
-                .os_tag = .freestanding,
-            }),
-            .optimize = b.standardOptimizeOption(.{}),
+    const cart = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
         }),
+        .optimize = b.standardOptimizeOption(.{}),
     });
 
+    const exe = b.addExecutable(.{
+        .name = "cart",
+        .root_module = cart,
+    });
     exe.entry = .disabled;
     exe.root_module.export_symbol_names = &[_][]const u8{ "start", "update" };
     exe.import_memory = true;
     exe.initial_memory = 65536;
     exe.max_memory = 65536;
     exe.stack_size = 14752;
-
     b.installArtifact(exe);
 
     const run_exe = b.addSystemCommand(&.{ "w4", "run-native" });
@@ -27,4 +27,11 @@ pub fn build(b: *std.Build) !void {
 
     const step_run = b.step("run", "compile and run the cart");
     step_run.dependOn(&run_exe.step);
+
+    const exe_check = b.addExecutable(.{
+        .name = "cart",
+        .root_module = cart
+    });
+    const check = b.step("check", "check if cart compiles");
+    check.dependOn(&exe_check.step);
 }
